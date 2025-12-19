e3ef4fe829a34ba43cecdfa445bd61846aa77f48
hihhojung-cpu <hihhojung@gmail.com>
2025-12-19 12:53:14 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,30 @@ def init_db():
 
 # 앱 시작할 때 DB 준비
 init_db()
+def add_post(content):
+    conn = sqlite3.connect("data.db")
+    c = conn.cursor()
+
+    c.execute(
+        "INSERT INTO posts (content, created_at) VALUES (?, ?)",
+        (content, datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    )
+
+    conn.commit()
+    conn.close()
+
 
 st.title(" 👾대원 에타 게시판")
+st.divider()
+
+content = st.text_area("💬 한마디 남기기", height=100)
+
+if st.button("등록"):
+    if content.strip():
+        add_post(content)
+        st.success("저장됐어!")
+        st.rerun()
+    else:
+        st.warning("내용을 입력해줘!")
+
 st.caption("익명으로 자유롭게 한마디를 남겨주세요‼️")
